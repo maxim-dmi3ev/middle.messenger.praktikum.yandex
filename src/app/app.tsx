4706bc2cadc7cc11a57d28ad5531351abcd6ab9e
Maxim <medmitriev@gmail.com>
2023-10-31 15:39:02 +0100

    страницы ошибок

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,14 +2,24 @@ import { Authorization } from "src/pages/authorization";
 import { Registration } from "src/pages/registration";
 import { Profile } from "src/pages/profile";
 import { Chat } from "../pages/chat";
+import { NotFound } from "../pages/not-found";
+import { ServerError } from "../pages/server-error";
 
 const ROUTES: Record<string, JSX.Element> = {
 	'/authorization': <Authorization />,
 	'/registration': <Registration />,
 	'/profile': <Profile />,
 	'/chat': <Chat />,
+	'/404': <NotFound />,
+	'/500': <ServerError /> ,
 };
 
 export const App = () => {
-	return ROUTES[location.pathname] || <div>404</div>;
+	const route = ROUTES[location.pathname];
+
+	if (!route) {
+		return <NotFound />;
+	}
+
+	return route;
 };
---
+++ src/components/error-view/error-view.tsx
@@ -0,0 +1,23 @@
+import styles from "./error-view.module.styl";
+import { Link } from "src/components/link";
+
+type TErrorViewProps = {
+	code: string;
+	message: string;
+	image: string;
+	link: string;
+	linkText: string;
+};
+
+export const ErrorView = ({ code, message, image, link, linkText }: TErrorViewProps) => {
+	return (
+		<div className={styles.error}>
+			<div className={styles.code}>{code}</div>
+			<div className={styles.message}>{message}</div>
+			<div className={styles.image}>
+				<img src={image} alt="image"/>
+			</div>
+			<Link to={link}>{linkText}</Link>
+		</div>
+	);
+};
